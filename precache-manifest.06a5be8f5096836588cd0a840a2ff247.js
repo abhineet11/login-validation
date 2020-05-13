decf4d869092d899365eebfc767fa15febdf99c9
apple <apple@apples-MacBook-Pro.local>
2020-05-13 22:13:41 +0530

    Updates

---
+++ precache-manifest.06a5be8f5096836588cd0a840a2ff247.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6d69ce98bc33a9c02a7379ad4fc7ee6",
+    "url": "/login-validation/index.html"
+  },
+  {
+    "revision": "13d65b892138fe387457",
+    "url": "/login-validation/static/css/main.4998a25d.chunk.css"
+  },
+  {
+    "revision": "87f14fdfeda883874053",
+    "url": "/login-validation/static/js/2.19afa4b6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/login-validation/static/js/2.19afa4b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13d65b892138fe387457",
+    "url": "/login-validation/static/js/main.b9aca7f2.chunk.js"
+  },
+  {
+    "revision": "c09173a6ba86c3941109",
+    "url": "/login-validation/static/js/runtime-main.90bba2f0.js"
+  }
+]);
